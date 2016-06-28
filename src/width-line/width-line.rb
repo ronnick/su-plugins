587ceb5676219a837fa2f62673d34358e9ef078a
ronnick <ronnick@163.com>
2016-06-29 00:36:36 +0800

    define methods.

--- src/width-line/width-line.rb
+++ src/width-line/width-line.rb
@@ -1,8 +1,31 @@
-require "offset.rb"
+
+def all_one_face(edges)
+  is_all_one_face = true
+  # find normal vector of a face
+  vect1 = edges[0].end.position - edges[0].start.position
+  vect2 = edges[1].end.position - edges[1].start.position
+  vect_fa = vect1.cross vect2
+  # check other edges
+  for i in (1...edges.length-1)
+    vect1 = edges[i].end.position - edges[i].start.position
+    vect2 = edges[i+1].end.position - edges[i+1].start.position
+    v_fa = vect1.cross vect2
+    unless v_fa.parallel? vect_fa
+      is_all_one_face = false
+      break
+    end
+  end # end of for
+  return is_all_one_face
+end # all_one_face
+
+
 
 model = Sketchup.active_model
 ents = model.entities
 sel = model.selection
+
+all_one_face sel
+
 =begin
 vect1 = sel[0].end.position - sel[0].start.position
 vect2 = sel[1].end.position - sel[1].start.position
@@ -19,19 +42,3 @@ trans = Geom::Transformation.new vect5
 
 ents.transform_entities trans, sel[]
 =end
-
-vect1 = sel[0].end.position - sel[0].start.position
-vect2 = sel[1].end.position - sel[1].start.position
-vect_fa = (vect1.cross vect2).normalize
-
-all_one_face = true
-
-for i in (1...sel.length-1)
-  vect3 = sel[i].end.position - sel[i].start.position
-  vect4 = sel[i+1].end.position - sel[i+1].start.position
-  v_f = (vect3.cross vect4).normalize
-  unless v_f.parallel? vect_fa
-    all_one_face = false
-  break
-  end
-end
